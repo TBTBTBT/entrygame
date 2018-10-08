7d9dc97a03dcd5b8d0644e5a4bf2b9634b4bd7af
Shogo Tabata <>
2018-10-09 00:32:35 +0900

    canvas

--- front/index.js
+++ front/index.js
@@ -1,5 +1,21 @@
 
 window.onload = function(){
-	var dialog = document.querySelector('dialog');
-	dialog.show();
+	//DialogOpen();
+}
+function DialogBack(){
+	var element_bg = document.createElement("div");
+	var style = element_bg.style;
+	style.position = "fixed";
+	style.left   = "0px";
+	style.right  = "0px";
+	style.top    = "0px";
+	style.bottom = "0px";
+	style.backgroundColor = "#000";
+	var opacity = 0.5;
+	style.opacity = opacity;
+	style.filter = "alpha(opacity=" + (opacity * 100) + ")";
+	document.body.appendChild(element_bg);
+}
+function DialogOpen(){
+	
 }
