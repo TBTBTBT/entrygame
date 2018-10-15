a5940a6df38ff893b30404f94e8d60d150a44226
田畑　翔伍 <shogo.tabata@sega.com>
2018-10-15 19:22:24 +0900

    ロジック

--- Gunner/Gunner/Assets/Script/Logic.cs
+++ Gunner/Gunner/Assets/Script/Logic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+//送受信する
 public class GunnerData
 {
     public int id;
@@ -11,35 +12,53 @@ public class GunnerData
     public int hp;
 
 }
-
-public class BulletData
+public enum InputType
 {
-    public int sFrame;//計算開始フレーム
-    public int eFeame;//計算終了フレーム
-    public int sx;//始点
-    public int sy;
-    public int ex;//終点 相手の弾によっても変化
-    public int ey;
-    public int id; // MstBulletRecord のID
-}
-public class MstBulletRecord{
-    public int id;
-    public int gravx;//重力
-    public int gravy;
-    public int weight;//速さに影響
-    public int atk;//攻撃力
-    public int atkRatio; //攻撃力増減
-    public int rad;//半径
-    public int radRatio;//半径増減
-    public int expRad;//爆発半径
+    BULLET = 1,
+    SKIP = 2,
+    FORWARD = 3,
+    BACKWARD = 4
 }
 public class InputData
 {
-    public int id;
-    public int type;
+    public int gunnerId;
+    public int bulletId;
+    public int inFrame;
+    public InputType type;
     public int strength;
     public int angle;
 }
+//送受信しない
+public class BulletData
+{
+    public int id; // MstBulletRecord のID
+    public int gunnerId; // 砲撃者のID
+    public int sFrame;//計算開始フレーム
+    public int eFrame = -1;//計算終了フレーム
+    public float strength;// 1/10
+    public float angle;// 1/10
+    public float cRad;
+    public Vector2 sPos;//始点
+    public Vector2 cPos;//現在の点
+   // public int ex;//終点 相手の弾によっても変化
+   // public int ey;
+
+}
+public class MstBulletRecord{
+    public int id;
+    public float gravx;//重力
+    public float gravy;
+    public float weight;//速さに影響 0~1 0が再重
+    public int atk;//攻撃力
+    public int atkRatio; //攻撃力増減
+    public float rad;//半径
+    public float radRatio;//半径増減
+    public float expRad;//爆発半径
+    public bool prevent;//弾同士ぶつかるか
+
+}
+
+
 public static class MasterDataManager{
     public static T Get<T>(int id) where T : class {
         if(typeof(T) == typeof(MstBulletRecord)){
@@ -48,7 +67,7 @@ public static class MasterDataManager{
         return null;
     }
     public static MstBulletRecord[] MstBulletRecordTable = {
-        new MstBulletRecord(){id = 10}
+        new MstBulletRecord(){id = 10,gravy = -10f,gravx = 0.95f,rad = 10,weight = 1f}
     };
 }
 
@@ -59,7 +78,8 @@ public class Logic
     private float SPF = 0.2f;//1計算fあたりの経過秒数
     //data
     private int _frameCount = 0;
-    private List<InputData> _log = new List<InputData>();
+    private List<InputData> _logInput = new List<InputData>();
+    private List<BulletData> _logBullet = new List<BulletData>();
     public List<GunnerData> Gunners { get; set; }
     public int FrameCount { get { return _frameCount; }}
     public void Init(List<GunnerData> gunners){
@@ -69,29 +89,124 @@ public class Logic
         }
         _frameCount = 0;
     }
+
+
+    void AddBullet(InputData input)
+    {
+        if (input.type == InputType.BULLET)
+        {
+            _logBullet.Add(InitBullet(input));
+        }
+    }
+
+    BulletData InitBullet(InputData input)
+    {
+        BulletData ret = new BulletData();
+        GunnerData gunner = Gunners.Find(_=>_.id == input.gunnerId);
+        if (gunner == null)
+        {
+            Debug.LogError("存在しないプレイヤーID");
+            return ret;
+        }
+        ret.sFrame = input.inFrame;
+        ret.eFrame = -1;
+        ret.id = input.bulletId;
+        ret.gunnerId = input.gunnerId;
+        ret.sPos = new Vector2(gunner.x, gunner.y);
+        ret.cPos = new Vector2(gunner.x, gunner.y);
+        ret.strength = input.strength;
+        ret.angle = (float)input.angle / 10;
+        ret.cRad = 0;
+        return ret;
+    }
+
+    bool IsNowCalculating(BulletData bullet)
+    {
+        return bullet.sFrame <= _frameCount && bullet.eFrame <= -1;
+    }
+    void CalcBullet()
+    {
+        List<BulletData> nowCalcBullet = _logBullet.FindAll(IsNowCalculating);
+        foreach (BulletData bullet in nowCalcBullet)
+        {
+            int frame = _frameCount - bullet.sFrame;
+            CalcOrbit(CalcTime(frame), bullet);
+        }
+    }
+
+    void CalcCollision()
+    {
+        List<BulletData> nowCalcBullet = _logBullet.FindAll(IsNowCalculating);
+        foreach (BulletData bullet in nowCalcBullet)
+        {
+            int frame = _frameCount - bullet.sFrame;
+            CalcLand(bullet);
+        }
+    }
+
+    void HitBullet()
+    {
+
+    }
+    void HitGunner()
+    {
+
+    }
+    void CalcLand(BulletData bullet)
+    {
+        if (bullet.cPos.y < 0)
+        {
+            bullet.eFrame = _frameCount;
+        }
+    }
+    void CalcOrbit(float time, BulletData bullet)
+    {
+        MstBulletRecord mstBullet = MasterDataManager.Get<MstBulletRecord>(bullet.id);
+        var gx = mstBullet.gravx;
+        var gy = mstBullet.gravy;
+        var wei = mstBullet.weight;
+        var sx = bullet.sPos.x;
+        var sy = bullet.sPos.y;
+        var str = bullet.strength;
+        var ang = bullet.angle;
+        var rad = mstBullet.rad;
+        float cx = sx + (time * bullet.strength * Mathf.Cos(ang * Mathf.Deg2Rad)) * wei;
+        float cy = sy + (time * bullet.strength * Mathf.Sin(ang * Mathf.Deg2Rad)) * wei;
+        cy += gy * time * time;
+//        cx *= gx;
+        bullet.cRad = rad;
+        bullet.cPos.x = cx;
+        bullet.cPos.y = cy;
+    }
     public float CalcTime(int frame)
     {
         return SPF * frame;
     }
-    public void CalcOrbit(float time, InputData input, GunnerData gunner, BulletData bullet)
-    {
-        var gx = bullet.gravx;
-        var gy = bullet.gravy;
-        var wei = bullet.weight;
-        var sx = gunner.x;
-        var sy = gunner.y;
-        var str = input.strength;
-        var ang = input.angle;
 
+    public void AddInput(InputData input)
+    {
+        _logInput.Add(input);
+        AddBullet(input);
     }
     public void NextFrame(){
         _frameCount++;
     }
 
-    public void CalcFrame()
+
+    public void Recalc(int pastFrame)
     {
-        
 
     }
-    public List<>
+
+    public void CalcFrame()
+    {
+        //        List<InputData> nowCalcInput = _logInput.FindAll(_=>_.inFrame == _frameCount);
+        CalcBullet();
+        CalcCollision();
+    }
+
+    public List<BulletData> HistoryBullets() => _logBullet;
+    public List<BulletData> NowBullets() => _logBullet.FindAll(IsNowCalculating);
+    
+//    public List<>
 }
--- Gunner/Gunner/Assets/Script/LogicManager.cs
+++ Gunner/Gunner/Assets/Script/LogicManager.cs
@@ -7,10 +7,75 @@ public class LogicManager : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         _logic = new Logic();
+        _logic.Init(new List<GunnerData>()
+        {
+            new GunnerData(){hp = 1000,id = 0,x = 100,y = 10},
+            new GunnerData(){hp = 1000,id = 1,x = -100,y = 10}
+        });
+	    StartCoroutine(UpdateLogic(0.2f));
 	}
-	
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+        //    _logic.NextFrame();
+        //    _logic.CalcFrame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            _logic.AddInput(new InputData()
+            {
+                angle = 1350,
+                bulletId = 10,
+                gunnerId = 0,
+                type = InputType.BULLET,
+                inFrame = _logic.FrameCount + 2,
+                strength = 100,
+            });
+        }
+    }
+
+    IEnumerator UpdateLogic(float span)
+    {
+        while (true)
+        {
+            _logic.NextFrame();
+            _logic.CalcFrame();
+            yield return new WaitForSeconds(span);
+        }
+    }
 	// Update is called once per frame
 	void OnGUI () {
-		
-	}
+		GUILayout.Label(_logic.FrameCount + " frame");
+	    foreach (var bullet in _logic.NowBullets())
+	    {
+	        GUILayout.Label($"[bullet] { bullet.cPos.x } , { bullet.cPos.y } ");
+	        DebugCircle(bullet.cPos,bullet.cRad, 20);
+        }
+	    DebugCircle(new Vector2(20,10),1, 5);
+
+    }
+
+    void DebugCircle(Vector2 center, float rad , int points)
+    {
+
+        GL.Begin(GL.LINE_STRIP);
+        GL.PushMatrix();
+        GL.LoadPixelMatrix();
+        center += new Vector2(Screen.width/2,Screen.height/2);
+        for (int i = 0; i < points; i++)
+        {
+            float radian = ((float)i / points) * Mathf.PI*2;
+            GL.Color(new Color(5,5,5,5));
+            // One vertex at transform position
+            //GL.Vertex3(0, 0, 0);
+            // Another vertex at edge of circle
+            
+            GL.Vertex3((center.x  + Mathf.Cos(radian) * rad) / 1, (center.y + Mathf.Sin(radian) * rad) / 1, 0);
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
 }
